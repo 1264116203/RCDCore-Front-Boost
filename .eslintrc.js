010dd1e0b5e1f85d66003b6d5cff2c93fc22f1da
limeng <limengwwwq@163.com>
2019-11-21 11:44:23 +0800

    添加内容

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,13 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/standard'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/html-self-closing': ['error', { 'html': { 'normal': 'any' } }]
   },
   parserOptions: {
     parser: 'babel-eslint'
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <a-locale-provider :locale="zhCN">
+    <div id="app"></div>
+  </a-locale-provider>
 </template>
 
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  name: 'App',
+  components: {},
+  data () {
+    return {
+      zhCN
+    }
+  },
+  created () {
+    if (!window.appShowed) {
+      window.showApp()
     }
   }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'PingFang SC', 'Microsoft YaHei', '微软雅黑', 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
 }
 </style>
