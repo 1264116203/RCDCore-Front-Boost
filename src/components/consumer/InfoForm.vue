5bedbcae092a3d26bd8bd003965a7968605b77e0
jiaojiao2 <97614742@qq.com>
2020-02-13 15:44:38 +0800

    添加上传头像功能

--- src/components/consumer/InfoForm.vue
+++ src/components/consumer/InfoForm.vue
@@ -4,19 +4,30 @@
             :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }"
     >
       <a-form-item label="头像" style="width: 60%" :label-col="{ span: 10 }" :wrapper-col="{ span: 10 }">
-        <a-upload-dragger
+        <a-upload
           v-decorator="['avatar']"
+          name="avatar"
+          list-type="picture-card"
+          class="avatar-uploader"
+          :headers="{
+            Authorization: 'Bearer ' + token
+          }"
+          :show-upload-list="false"
+          action="/api/upload?subPath=avatar"
+          :before-upload="beforeUpload"
+          @change="onChangeAvatar"
         >
-          <p class="ant-upload-drag-icon">
-            <a-icon type="plus" />
-          </p>
-          <p class="ant-upload-text">
-            Click or drag file to this area to upload
-          </p>
-          <p class="ant-upload-hint">
-            Support for a single or bulk upload.
-          </p>
-        </a-upload-dragger>
+          <a-badge v-if="imageUrl" @click.stop="onCancelAvatar">
+            <a-icon type="close" style="color: #f5222d" />
+          </a-badge>
+          <img v-if="imageUrl" :src="imageUrl" alt="avatar" class="avatar-img">
+          <div v-else>
+            <a-icon :type="loading ? 'loading' : 'plus'" />
+            <div class="ant-upload-text">
+              点击上传
+            </div>
+          </div>
+        </a-upload>
       </a-form-item>
       <a-form-item label="姓名" style="width: 100%" :label-col="{ span: 6 }" :wrapper-col="{ span: 10 }">
         <a-input
@@ -69,13 +80,24 @@
 
 <script>
 import { getUserInfo, updateUserInfo } from '@/api/common'
+import { mapGetters } from 'vuex'
+
+function getBase64(img, callback) {
+  const reader = new FileReader()
+  reader.addEventListener('load', () => callback(reader.result))
+  reader.readAsDataURL(img)
+}
 export default {
   data() {
     return {
-      type: 'info',
       form: this.$form.createForm(this),
-      currentId: ''
+      currentId: '',
+      loading: false,
+      imageUrl: ''
     }
+  },
+  computed: {
+    ...mapGetters(['token'])
   },
   created() {
     this.doSwitch()
@@ -84,6 +106,7 @@ export default {
     onSubmit() {
       const formData = {
         ...this.form.getFieldsValue(),
+        avatar: this.form.getFieldValue('avatar').fileList[0].response.url,
         id: this.currentId
       }
       updateUserInfo(formData).then(res => {
@@ -106,10 +129,48 @@ export default {
           email: user.email
         })
       })
-    } }
+    },
+    onChangeAvatar(info) {
+      if (info.file.status === 'uploading') {
+        this.loading = true
+        return
+      }
+      if (info.file.status === 'done') {
+        // Get this url from response in real world.
+        getBase64(info.file.originFileObj, imageUrl => {
+          this.imageUrl = imageUrl
+          this.loading = false
+        })
+      }
+    },
+    beforeUpload(file) {
+      const isJPG = file.type === 'image/jpeg'
+      if (!isJPG) {
+        this.$message.error('You can only upload JPG file!')
+      }
+      const isLt2M = file.size / 1024 / 1024 < 200
+      if (!isLt2M) {
+        this.$message.error('Image must smaller than 2MB!')
+      }
+      return isJPG && isLt2M
+    },
+    onCancelAvatar() {
+      this.imageUrl = ''
+    }
+  }
 }
 </script>
 
 <style>
+.avatar-uploader .avatar-img{
+    width: 128px;
+    height: 128px;
+  }
+
+.ant-upload .ant-badge{
+    position: absolute;
+    left: 138px;
+    top: 138px;
+  }
 
 </style>
