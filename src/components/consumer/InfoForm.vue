7554e589c4d20e060315088827d8b91649dde662
jiaojiao2 <97614742@qq.com>
2020-02-17 10:24:29 +0800

    上传头像抽成组件（UploadAvatar）

--- src/components/consumer/InfoForm.vue
+++ src/components/consumer/InfoForm.vue
@@ -4,30 +4,7 @@
             :label-col="{ span: 3 }" :wrapper-col="{ span: 21 }"
     >
       <a-form-item label="头像">
-        <a-upload
-          ref="avatarUpload"
-          name="avatar"
-          list-type="picture-card"
-          class="avatar-uploader"
-          action="/api/upload?subPath=avatar"
-          :headers="{ Authorization: 'Bearer ' + token }"
-          :show-upload-list="false"
-          :before-upload="beforeUpload"
-          @change="onChangeAvatar"
-        >
-          <div style="position: relative;">
-            <a-badge v-if="imageUrl" @click.stop="onCancelAvatar">
-              <a-icon type="close-circle" theme="filled" style="color: #f5222d" />
-            </a-badge>
-            <img v-if="imageUrl" :src="imageUrl" class="avatar-img">
-            <div v-else>
-              <a-icon :type="loading ? 'loading' : 'plus'" />
-              <div class="ant-upload-text">
-                点击上传
-              </div>
-            </div>
-          </div>
-        </a-upload>
+        <upload-avatar :action="action" :image-url.sync="imageUrl" :avatar-url="imageUrl" />
       </a-form-item>
       <a-form-item label="姓名">
         <a-input
@@ -81,20 +58,19 @@
 
 <script>
 import { getUserInfo, updateUserInfo } from '@/api/common'
-import { mapGetters } from 'vuex'
+import UploadAvatar from '@/components/consumer/UploadAvatar'
 
 export default {
+  components: {
+    UploadAvatar
+  },
   data() {
     return {
       form: this.$form.createForm(this),
       currentId: '',
-      loading: false,
       imageUrl: '',
-      avatarFile: null
+      action: '/api/upload?subPath=avatar'
     }
-  },
-  computed: {
-    ...mapGetters(['token'])
   },
   created() {
     this.fetchData()
@@ -127,54 +103,11 @@ export default {
         })
         this.imageUrl = user.avatar
       })
-    },
-    onChangeAvatar({ file, fileList, event }) {
-      if (file.status === 'uploading') {
-        this.loading = true
-        if (this.avatarFile) {
-          this.$refs.avatarUpload.handleManualRemove(this.avatarFile)
-          this.avatarFile = null
-        }
-        return
-      }
-      if (file.status === 'done') {
-        this.imageUrl = file.response.url
-        this.loading = false
-        this.avatarFile = file
-      }
-    },
-    beforeUpload(file) {
-      const isJPG = file.type === 'image/jpeg'
-      if (!isJPG) {
-        this.$message.error('You can only upload JPG file!')
-      }
-      const isLt2M = file.size / 1024 / 1024 < 200
-      if (!isLt2M) {
-        this.$message.error('Image must smaller than 2MB!')
-      }
-      return isJPG && isLt2M
-    },
-    onCancelAvatar() {
-      this.imageUrl = null
-      if (this.avatarFile) {
-        this.$refs.avatarUpload.handleManualRemove(this.avatarFile)
-        this.avatarFile = null
-      }
     }
   }
 }
 </script>
 
 <style>
-.avatar-uploader .avatar-img {
-  display: block;
-  max-width: 128px;
-}
-
-.ant-upload .ant-badge {
-  position: absolute;
-  right: -5px;
-  top: -5px;
-}
 
 </style>
---
+++ src/components/consumer/UploadAvatar.vue
@@ -0,0 +1,106 @@
+<template>
+  <a-upload
+    ref="avatarUpload"
+    name="avatar"
+    list-type="picture-card"
+    class="avatar-uploader"
+    :action="action"
+    :headers="{ Authorization: 'Bearer ' + token }"
+    :show-upload-list="false"
+    :before-upload="beforeUpload"
+    @change="onChangeAvatar"
+  >
+    <div style="position: relative;">
+      <a-badge v-if="imageUrl" @click.stop="onCancelAvatar">
+        <a-icon type="close-circle" theme="filled" style="color: #f5222d" />
+      </a-badge>
+      <img v-if="imageUrl" :src="imageUrl" class="avatar-img">
+      <div v-else>
+        <a-icon :type="loading ? 'loading' : 'plus'" />
+        <div class="ant-upload-text">
+          点击上传
+        </div>
+      </div>
+    </div>
+  </a-upload>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  props: {
+    action: {
+      type: String,
+      required: true
+    },
+    avatarUrl: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      loading: false,
+      avatarFile: null
+    }
+  },
+  computed: {
+    ...mapGetters(['token']),
+    imageUrl() {
+      return this.avatarUrl
+    }
+  },
+  methods: {
+    onChangeAvatar({ file, fileList, event }) {
+      if (file.status === 'uploading') {
+        this.loading = true
+        if (this.avatarFile) {
+          this.$refs.avatarUpload.handleManualRemove(this.avatarFile)
+          this.avatarFile = null
+        }
+        return
+      }
+      if (file.status === 'done') {
+        // this.imageUrl = file.response.url
+        this.$emit('update:imageUrl', file.response.url)
+        this.loading = false
+        this.avatarFile = file
+      }
+    },
+    beforeUpload(file) {
+      const isJPG = file.type === 'image/jpeg'
+      if (!isJPG) {
+        this.$message.error('You can only upload JPG file!')
+      }
+      const isLt2M = file.size / 1024 / 1024 < 200
+      if (!isLt2M) {
+        this.$message.error('Image must smaller than 2MB!')
+      }
+      return isJPG && isLt2M
+    },
+    onCancelAvatar() {
+    //   this.imageUrl = null
+      this.$emit('update:imageUrl', null)
+      if (this.avatarFile) {
+        this.$refs.avatarUpload.handleManualRemove(this.avatarFile)
+        this.avatarFile = null
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.avatar-uploader .avatar-img {
+  display: block;
+  max-width: 128px;
+}
+
+.ant-upload .ant-badge {
+  position: absolute;
+  right: -5px;
+  top: -5px;
+}
+
+</style>
