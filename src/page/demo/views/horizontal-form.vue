73c90fe60b818fcc189434587579c34b63e21846
jiaojiao2 <97614742@qq.com>
2020-04-01 14:11:43 +0800

    给纵向表单左边文字固定宽示例加概述

--- src/page/demo/views/horizontal-form.vue
+++ src/page/demo/views/horizontal-form.vue
@@ -2,6 +2,70 @@
   <a-card style="height: 100%">
     <h3>纵向表单左边文字固定宽示例</h3>
 
+    <a-card>
+      <div class="markdown">
+        <h2 id="-">
+          概述
+        </h2>
+        <p>纵向表单列，自定义左边固定宽度，右边内容自由缩放</p>
+        <ul>
+          <li>
+            表单布局：<a>layout</a>设置为<a>horizontal</a>，也是表单默认的布局
+          </li>
+          <li>
+            <a>formItemLayout</a>：通过计算属性得到，与表单的<a>label-col</a>、<a>wrapper-col</a>绑定
+          </li>
+          <li>
+            <a>labelCol</a>：通过计算属性<a>formItemLayout</a>中设置它的style属性，可以实现自定义左边固定宽度，与data中<a>itemWidth</a>绑定
+          </li>
+          <li>
+            多列栅格式的表单排列方式：通过修改<a>ant-form-item</a>样式<a>width</a>的百分比调整列数。如:100%单列；50%两列；33%三列；以此类推。
+          </li>
+        </ul>
+        <strong>注意：</strong>
+        <br>
+        标签宽度（px），绑定的是data中<a>itemWidth</a>，可以直观调整自己想要的宽度
+        <br>
+        <a>labelCol</a>中，不可以同时设置<a>style</a>和<a>span</a>，否则失去自定义固定宽度的效果
+        <br>
+        调整<a>width</a>为多列时，可以搭配设置<a>padding-left</a>的值使布局更完美一些
+      </div>
+      <h3>
+        主要代码显示
+      </h3>
+      <textarea style="width:200px;height:200px">
+        /* ---示例代码 a-form----*/
+        <a-form
+        layout="horizontal"
+        :style="{width: formWidth + 'px'}"
+        :label-col="formItemLayout.labelCol"
+        :wrapper-col="formItemLayout.wrapperCol"
+        style="border: 1px dashed orange"
+      />
+      </textarea>
+      <textarea style="width:200px;height:200px">
+        /* ---示例代码 计算属性formItemLayout----*/
+        computed: {
+            formItemLayout() {
+                return {
+                    labelCol: { style: `min-width: ${this.itemWidth}px` },
+                    wrapperCol: { style: { flex: '1 1 auto' } }
+                }
+            }
+        }
+      </textarea>
+      <textarea style="width:200px;height:200px">
+        /* ---示例代码 样式设置----*/
+        .ant-form-horizontal{
+            .ant-form-item{
+                display: inline-flex;
+                width: 100%;
+                // padding-left: 1rem;
+            }
+        }
+      </textarea>
+    </a-card>
+
     <a-row>
       <a-form-item label="表单宽度（px）">
         <a-row :gutter="24">
